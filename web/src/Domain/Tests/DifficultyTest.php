189396a9f03baa1ae1ce78bee3194c9474e7c7d0
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2020-03-21 15:31:36 +0100

    Add tests for difficulty

---
+++ web/src/Domain/Tests/DifficultyTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace HelloFresh\Domain\Tests;
+
+use HelloFresh\Domain\Difficulty;
+use PHPUnit\Framework\TestCase;
+
+final class DifficultyTest extends TestCase
+{
+    /**
+     * @dataProvider validRangeValueProvider
+     * @testdox Can create difficulty level with $validValue
+     *
+     * @param int $validValue
+     */
+    public function testCanCreateFromValidRangeOfLevels(int $validValue): void
+    {
+        $this->assertInstanceOf(Difficulty::class, $difficultLevel = Difficulty::fromInteger($validValue));
+        $this->assertEquals($difficultLevel->toInteger(), $validValue);
+    }
+
+    /**
+     * @dataProvider outOfRangeLevelsProvider
+     * @expectedException \HelloFresh\Domain\Exception\OutOfRangeDifficultyLevelException
+     * @testdox Can't create difficulty level with $outOfRangeValue
+     *
+     * @param int $outOfRangeValue
+     */
+    public function testCantCreateFromOutOfRangeLevels(int $outOfRangeValue): void
+    {
+        Difficulty::fromInteger($outOfRangeValue);
+    }
+
+    public function outOfRangeLevelsProvider(): array
+    {
+        return [
+            [-2],
+            [0],
+            [4],
+        ];
+    }
+
+    public function validRangeValueProvider(): array
+    {
+        $values = [];
+
+        foreach (range(1, 3) as $value) {
+            array_push($values, [$value]);
+        }
+
+        return $values;
+    }
+}
